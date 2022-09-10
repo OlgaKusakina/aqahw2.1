ad8639cfdec03b7d9c44b939ad12b8c5ed921ef7
Olga Kusakina <kusakina_olya@mail.ru>
2022-09-10 18:38:05 +0300

    test Passed

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+
 [![Build status](https://ci.appveyor.com/api/projects/status/e148i9wk8sviqlkh?svg=true)](https://ci.appveyor.com/project/OlgaKusakina/aqahw2-1)
---
+++ src/test/java/OrderTest.java
@@ -0,0 +1,23 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class OrderTest {
+    @Test
+     public void Form(){
+      Configuration.holdBrowserOpen=true;
+      open("http://localhost:9999/");
+      $("[name='name'").setValue("Иванов Иван");
+      $("[name='phone'").setValue("+79779999999");
+      $("[role='presentation']").click();
+      $(byText("Продолжить")).click();
+      $("[data-test-id='order-success']").shouldHave(Condition.exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+
+
+
+}
+}
